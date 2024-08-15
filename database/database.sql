dcb3680723a2c4414044bf3a71a319a3c849bd36
Alvin Graham <grah0071@gmail.com>
2024-08-14 20:19:36 -0500

    Add additional tables to support detail view

--- database/database.sql
+++ database/database.sql
@@ -1,18 +1,56 @@
 --Database should be called fire_db
 
--- Create the table
-CREATE TABLE locations (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    city VARCHAR(100),
-    state CHAR(2),
-    photo TEXT,
-    available_units INT,
-    healthcare_score VARCHAR(50),
-    laundry BOOLEAN,
-    col INT,
-    population INT
+-- Create the tables
+CREATE TABLE IF NOT EXISTS "locations" (
+	"id" bigint GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	"name" text NOT NULL,
+	"city" text NOT NULL,
+	"state" text NOT NULL,
+	"photo" text NOT NULL,
+	"available_units" bigint NOT NULL,
+	"healthcare_score" text NOT NULL,
+	"laundry" boolean NOT NULL,
+	"col" bigint NOT NULL,
+	"population" bigint NOT NULL,
+	PRIMARY KEY ("id")
 );
+
+CREATE TABLE IF NOT EXISTS "about" (
+	"id" bigint GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	"location_id" bigint NOT NULL,
+	"col_narrative" text,
+	"transport_narrative" text,
+	"system_narrative" text,
+	"health_narrative" text,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "transport_bullets" (
+	"id" bigint GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	"about_id" bigint NOT NULL,
+	"bullet" text,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "system_bullets" (
+	"id" bigint GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	"about_id" bigint NOT NULL,
+	"bullet" text,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "healthcare_bullets" (
+	"id" bigint GENERATED ALWAYS AS IDENTITY NOT NULL UNIQUE,
+	"about_id" bigint NOT NULL,
+	"bullet" text,
+	PRIMARY KEY ("id")
+);
+
+
+ALTER TABLE "about" ADD CONSTRAINT "about_fk1" FOREIGN KEY ("location_id") REFERENCES "locations"("id");
+ALTER TABLE "transport_bullets" ADD CONSTRAINT "transport_bullets_fk1" FOREIGN KEY ("about_id") REFERENCES "about"("id");
+ALTER TABLE "system_bullets" ADD CONSTRAINT "system_bullets_fk1" FOREIGN KEY ("about_id") REFERENCES "about"("id");
+ALTER TABLE "healthcare_bullets" ADD CONSTRAINT "healthcare_bullets_fk1" FOREIGN KEY ("about_id") REFERENCES "about"("id");
 
 -- Insert the data into the table
 INSERT INTO locations (name, city, state, photo, available_units, healthcare_score, laundry, col, population)
