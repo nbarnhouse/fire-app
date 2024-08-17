c9c9d55ad51955bc369cbcaf3c56863e416961fc
Alvin Graham <grah0071@gmail.com>
2024-08-16 21:39:18 -0500

    Load location data prior to rendering page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../Home/Home';
-import About from '../About/About';
-import CitiesRanked from '../CitiesRanked/CitiesRanked';
-import Resources from '../Resources/Resources';
-import Community from '../Community/Community';
-import PageLayout from '../PageLayout/PageLayout';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Home from "../Home/Home";
+import About from "../About/About";
+import CitiesRanked from "../CitiesRanked/CitiesRanked";
+import Resources from "../Resources/Resources";
+import Community from "../Community/Community";
+import PageLayout from "../PageLayout/PageLayout";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
+  const dispatch = useDispatch();
+  const locations = useSelector((store) => store.locations);
+  const [locationsReady, setLocationsReady] = useState(false);
+
+  useEffect(() => {
+    dispatch({ type: "GET_LOCATIONS" });
+  }, []);
+
+  useEffect(() => {
+    if (locations.loading === false) {
+      setLocationsReady(true);
+      console.log("Data Loaded...");
+    }
+  }, [locations.loading]);
+
+  if (!locationsReady) {
+    console.log("Loading Data");
+    return <h1>Loading Data...</h1>;
+  }
   return (
     <BrowserRouter>
       <PageLayout>
--- src/redux/sagas/locations.saga.js
+++ src/redux/sagas/locations.saga.js
@@ -1,13 +1,11 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-function* getLocations(action) {
+function* getLocations() {
   try {
     yield put({ type: "CLEAR_LOCATIONS" });
-
     const response = yield axios.get("/api/data");
-
-    yield put({ type: "SET_LOCATIONs", payload: response.data });
+    yield put({ type: "SET_LOCATIONS", payload: response.data });
   } catch (error) {
     console.error("ERROR setting locations:", err);
   }
